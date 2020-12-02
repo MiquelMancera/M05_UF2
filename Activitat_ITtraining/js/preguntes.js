9c9f1422d81edc002981b5d10f5d43a2d7a1bc42
Miquel Mancera <miquelmancera@ginebro.cat>
2020-12-02 18:48:42 +0100

    Edició en el js

--- Activitat_ITtraining/js/preguntes.js
+++ Activitat_ITtraining/js/preguntes.js
@@ -8,7 +8,36 @@ function pregunta(objecte, numPreg){
             resposta = i + 1;
         }
     }
-
     arrRespostes[numPreg] = resposta;
     alert ("has escollit la resposta "+ resposta + " de la pregunta " + (numPreg+1));
+
+    switch(numPreg){
+        case 0:
+            obj = document.getElementById("pregunta-1");
+            obj.classList.remove("elementVisible");
+            obj.classList.add("elementOcult");
+
+            obj = document.getElementById("pregunta-2");
+            obj.classList.remove("elementOcult");
+            obj.classList.add("elementVisible");
+        break;
+        case 1:
+            obj = document.getElementById("pregunta-2");
+            obj.classList.remove("elementVisible");
+            obj.classList.add("elementOcult");
+
+            obj = document.getElementById("pregunta-3");
+            obj.classList.remove("elementOcult");
+            obj.classList.add("elementVisible");
+        break;
+        case 2:
+            obj = document.getElementById("pregunta-3");
+            obj.classList.remove("elementVisible");
+            obj.classList.add("elementOcult");
+
+            obj = document.getElementById("pregunta-4");
+            obj.classList.remove("elementOcult");
+            obj.classList.add("elementVisible");
+        break;
+    }
 }
